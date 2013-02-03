ba92a4c1b1aa39b66a8c68051048146c3807e738
Sebastian Cabrera <scabrera-github@portachtzig.de>
2013-02-03 01:37:22 +0100

    work in progress...

--- inventorize_cli.py
+++ inventorize_cli.py
@@ -6,11 +6,7 @@
 from optparse import OptionParser
 from ConfigParser import RawConfigParser
 from sys import exit
-
-# Main Variables
-modulepath = None
-inventorize_server = None
-modules_enabled = None
+from sys import path as sys_path
 
 parser = OptionParser() # {{{
 
@@ -21,45 +17,51 @@ options, args = parser.parse_args()
 # }}}
 
 def parsedata(): # {{{
-    global inventorize_server, modulepath, modules_enabled
+    configdict = {}
     if not confparser.has_section('Main'):
         raise StandardError('No Main Section found in configfile...')
     else:
         if confparser.has_option('Main', 'modulepath'):
-            modulepath = confparser.get('Main', 'modulepath')
+            sys_path.append(confparser.get('Main', 'modulepath'))
         else:
             print 'Please give a valid modulepath in configfiles Main Section'
             exit(255)
 
         if confparser.has_option('Main', 'inventorize_server'):
-            inventorize_server = confparser.get('Main', 'inventorize_server')
+            configdict.update({ 'inventorize_server' : confparser.get('Main', 'inventorize_server') })
         else:
             print 'Please give a valid inventorize Server'
             exit(255)
 
         if confparser.has_option('Main', 'modules_enabled'):
-            modules_enabled = confparser.get('Main', 'modules_enabled').split()
+            configdict.update({ 'modules_enabled' : confparser.get('Main', 'modules_enabled').split() })
+    return configdict
 # }}}
 
 def get_arguments(configsection): # {{{
+    argumentdict = {}
     if confparser.has_section(configsection):
-        pass
+        for option in confparser.options(configsection):
+            argumentdict.update({ option : confparser.get(configsection, option) })
     else:
-        print 'activated module is not available'
+        print 'activated module has no options'
         exit(255)
+    return argumentdict
 # }}}
 
-def load_modules(modules):
+def load_modules(modules): # {{{
     if modules == None:
         exit(0)
     else:
-        for module in modules:
-            arguments = get_arguments(module)
-            try:
-                active_module = __import__(modulepath + '/' + module)
-            except:
-                raise StandardError('you need to have the module in place')
+        try:
+            return __import__(module)
+        except:
+            raise StandardError('you need to have the module in place')
+# }}}
  
+def send_data(inventorize_server, result): # {{{
+    pass
+# }}}
 
 if __name__ == '__main__':
     if not options.configfile:
@@ -67,5 +69,9 @@ if __name__ == '__main__':
     else:
         confparser = RawConfigParser()
         confparser.read(options.configfile)
-        parsedata()
-        load_modules(modules_enabled)
+        configdict = parsedata()
+        for module in configdict['modules_enabled']:
+            arguments = get_arguments(module)
+            active_module = load_modules(module)
+            result = active_module.run(arguments)
+            send_data(configdict['inventorize_server'], result)
